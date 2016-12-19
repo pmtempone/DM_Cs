5236386b3ea2e8a73e62887a5defe5557851227f
pmtempone <pmtempone@gmail.com>
2016-12-19 17:16:46 -0300

    tp 2 parte 2 ultimos cambios

--- tp2_parte2.R
+++ tp2_parte2.R
@@ -22,6 +22,36 @@ points(uniq_checks_noruega$lon, uniq_checks_noruega$lat, col = "red", cex =
          .7, pch=20)
 
 nor_map_r <- getMap(resolution = "low")
-plot(nor_map_r, xlim = c(10, 20), ylim = c(44, 85), asp = 1)
-points(uniq_checks_rusia$lon, uniq_checks_rusia$lat, col = "red", cex =
+plot(nor_map_r, xlim = c(30, 31), ylim = c(45, 75), asp = 1)
+points(uniq_checks_noruega$lon, uniq_checks_noruega$lat, col = "red", cex =
          .7, pch=20)
+
+
+install.packages("ggmap")
+library("ggmap")
+noruega_mapa_1 <- get_map(location = c(17,66), zoom = 4)
+ggmap(noruega_mapa_1)
+noruega_checkin_map_1 <- ggmap(noruega_mapa_1) +
+  geom_point(aes(x = lon, y = lat),
+             alpha = 0.5,
+             data = uniq_checks_noruega)
+noruega_checkin_map_1
+
+
+noruega_checkin_map_2 <- ggmap(noruega_mapa_1) +
+  geom_point(aes(x = lon, y = lat, col = n),
+             alpha = 0.5,
+             data = uniq_checks_noruega) +
+  scale_color_gradient(low="blue", high="red")
+noruega_checkin_map_2
+
+
+# install.packages("fossil")
+library(fossil)
+noruega_dist <- earth.dist(checkins_noruega[,4:3]) #ojo con la memoria!
+plot(hclust(noruega_dist))
+rusia_clusts <- cutree(hclust(rusia_dist), h = 20)
+unique(rusia_clusts)
+checkins_rusia <- data.frame(checkins_rusia, geo_cluster = rusia_clusts)
+cluster_cons <- checkins_rusia %>% group_by(geo_cluster) %>%
+  summarize(lat=mean(lat), lon=mean(lon))
